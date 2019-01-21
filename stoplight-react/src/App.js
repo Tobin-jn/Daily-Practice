3f2672efee79f631cbd5672427ebe32a41814a3b
Tobin-jn <tobin.j.n@gmail.com>
2019-01-21 09:58:31 -0700

    Version 1

--- stoplight-react/src/App.js
+++ stoplight-react/src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      red: false,
+      yellow: false,
+      green: false,
+    }
+  }
+
+  componentDidMount(){
+    this.changeColors()
+  }
+
+  changeColors = () => {
+    setTimeout(this.changeRed, 5000)
+    setTimeout(this.changeYellow, 7000)
+    setTimeout(this.changeGreen, 9000)
+  }
+
+  changeRed = () => {
+    this.setState({ red: true })
+  }
+
+  changeYellow = () => {
+    this.setState({ red: false, yellow: true })
+  }
+
+  changeGreen = () => {
+    this.setState({ yellow: false, green: true })
+  }
+
   render() {
+    let light = 'light'
+    let redClass = 'light red'
+    let yellowClass = 'light yellow'
+    let greenClass = 'light green'
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className= { this.state.red ? redClass : light }> </div>
+        <div className= { this.state.yellow ? yellowClass : light }> </div>
+        <div className= { this.state.green ? greenClass : light }> </div>
       </div>
     );
   }
