7e68104951772c6d5c089419e2fceff9777bab5c
Tobin-jn <tobin.j.n@gmail.com>
2019-01-22 21:38:43 -0700

    All passing except delete

--- binary-search-tree/tests/test.js
+++ binary-search-tree/tests/test.js
@@ -12,11 +12,11 @@ describe('BinaryTree', () => {
     expect(tree).to.be.an('object')
   })
 
-  it.skip('should have a rootNode node defaulted to null', () => {
+  it('should have a rootNode node defaulted to null', () => {
     expect(tree.rootNode).to.equal(null);
   });
 
-  describe.skip('insert', () => {
+  describe('insert', () => {
     it('should be able to add a node to the Tree', () => {
       tree.insert(20);
 
@@ -65,7 +65,7 @@ describe('BinaryTree', () => {
     });
   });
 
-  describe.skip('min and max', () => {
+  describe('min and max', () => {
     it('min should return null if no nodes exist', () => {
       let tree = new BinaryTree();
 
@@ -97,7 +97,7 @@ describe('BinaryTree', () => {
     });
   });
 
-  describe.skip('find', () => {
+  describe('find', () => {
     beforeEach(() => {
       tree.insert(4);
       tree.insert(6);
@@ -108,37 +108,37 @@ describe('BinaryTree', () => {
       tree.insert(1);
     });
 
-    it.skip('should return null if no match is found', () => {
+    it('should return null if no match is found', () => {
       let node = tree.find(10)
 
       expect(node).to.equal(null);
     });
 
-    it.skip('should be able to find the rootNode', () => {
+    it('should be able to find the rootNode', () => {
       let node = tree.find(4);
 
       expect(node).to.equal(tree.rootNode);
     });
 
-    it.skip('should be able to find results to the immediate left (2)', () => {
+    it('should be able to find results to the immediate left (2)', () => {
       let node = tree.find(2);
 
       expect(node).to.equal(tree.rootNode.left);
     });
 
-    it.skip('should be able to find results to the far left (1)', () => {
+    it('should be able to find results to the far left (1)', () => {
       let node = tree.find(1);
 
       expect(node).to.equal(tree.rootNode.left.left);
     });
 
-    it.skip('should be able to find results to the immediate right (6)', () => {
+    it('should be able to find results to the immediate right (6)', () => {
       let node = tree.find(6);
 
       expect(node).to.equal(tree.rootNode.right);
     });
 
-    it.skip('should be able to find nested results (5 and 3)', () => {
+    it('should be able to find nested results (5 and 3)', () => {
       let node = tree.find(5);
 
       expect(node).to.equal(tree.rootNode.right.left);
