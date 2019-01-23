2ce643e0d1a467a7eb713298b107de5f8c0b8036
Tobin-jn <tobin.j.n@gmail.com>
2019-01-23 13:03:36 -0700

    Build basic structure

--- tip-calculator-react/src/App.js
+++ tip-calculator-react/src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      tipPercent: null,
+      totalBill: null,
+      totalTip: null,
+      totalBillWithTip: null,
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="body">
+          <h1>Tip Calculator</h1>
+          <form action="">
+            Desired Tip Percent: <input type="text" className="percentage"/>
+            Total Bill: <input type="text" className="total"/>
+          </form>
+          <div className="results">
+            <h3>Tip</h3>
+            <p>Total Tip Amount: {/* Output Tip */} </p>
+            <p>Total Bill with Tip Amount: {/* Output Tip */}</p>
+          </div>
+        </section>
       </div>
     );
   }
