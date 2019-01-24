4ccf504711cfc869b5a9c1b7e86c13966276a071
Tobin-jn <tobin.j.n@gmail.com>
2019-01-24 06:15:01 -0700

    All tip logic

--- tip-calculator-react/src/App.js
+++ tip-calculator-react/src/App.js
@@ -5,10 +5,33 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      tipPercent: null,
-      totalBill: null,
-      totalTip: null,
-      totalBillWithTip: null,
+      tipPercent: '',
+      totalBill: '',
+      totalTip: '',
+      totalBillWithTip: '', 
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleTip = this.handleTip.bind(this);
+  }
+
+  async handleChange(event){
+    const { name, value } = event.target
+    await this.setState({
+      [name]: value
+    })
+    this.handleTip()
+  }
+
+  handleTip(event){
+    const { tipPercent, totalBill} = this.state
+    if(tipPercent.length !== 0 && totalBill.length !== 0){
+      const bill = parseFloat(totalBill)
+      const tip = parseFloat(tipPercent)/100
+      this.setState({
+        totalTip: (bill*tip).toFixed(2),
+        totalBillWithTip: (bill + (bill*tip)).toFixed(2)
+      })
     }
   }
 
@@ -18,13 +41,27 @@ class App extends Component {
         <section className="body">
           <h1>Tip Calculator</h1>
           <form action="">
-            Desired Tip Percent: <input type="number" className="percentage"/>
-            Total Bill: <input type="number" className="total"/>
+            Desired Tip Percent: <input 
+              type="number" 
+              className="percentage"
+              placeholder="20%"
+              name='tipPercent'
+              value={this.state.tipPercent}
+              onChange={this.handleChange}
+              />
+            Total Bill: <input 
+              type="number" 
+              className="total"
+              placeholder='$100.00'
+              name='totalBill'
+              value={this.state.totalBill}
+              onChange={this.handleChange}
+              />
           </form>
           <div className="results">
             <h3>Tip</h3>
-            <p>Total Tip Amount: {/* Output Tip */} </p>
-            <p>Total Bill with Tip Amount: {/* Output Tip */}</p>
+            <p>Total Tip Amount: {this.state.totalTip} </p>
+            <p>Total Bill with Tip Amount: {this.state.totalBillWithTip}</p>
           </div>
         </section>
       </div>
