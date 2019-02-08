e61bf0fbaa6ab6c6df01e8d66c6462a42074d482
Tobin-jn <tobin.j.n@gmail.com>
2019-02-08 15:30:03 -0700

    Updates for styling

--- resolutions-react/src/Resolution/index.js
+++ resolutions-react/src/Resolution/index.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import ContentEditable from '../ContentEditable'
+import '../App/App.css';
 
 const Resolution = (props) => {
+
   const tags = props.tags.map( tag => {
-    return <p className='tag'>{tag}</p>
+    let tagStyle;
+ 
+    switch(tag){
+      case 'Health':
+        tagStyle = {backgroundColor: '#d0efd3'};
+        break;
+      case 'Work':
+        tagStyle = {backgroundColor: '#e25f70'};
+        break;
+      case 'Family':
+        tagStyle = {backgroundColor: '#e7732f'};
+        break;
+      case 'Good Habits':
+        tagStyle = {backgroundColor: '#dae9e4'};
+        break;
+      case 'Bad Habits':
+        tagStyle = {backgroundColor: '#6cbf84'};
+        break;
+      case 'Friends':
+        tagStyle = {backgroundColor: '#ca7f4c'};
+        break;
+      case 'Fun':
+        tagStyle = {backgroundColor: '#895091'};
+        break;
+    }
+    return <p className='resolution__tag' style={tagStyle} >{tag}</p>
   })
 
   let EditableH1 = ContentEditable('h1') 
   let EditableH3 = ContentEditable('h3') 
+  let statusClass;
 
-    return(
-    <div 
-      className="resolution"
-    >
-      <EditableH1 className='title' value={props.title} />
-      <EditableH3 className='description' value={props.description} />
-      <h4 className='status'>{props.status}</h4>
-      { tags }
+  if (props.status==='Completed'){
+    statusClass = 'resolution__status--completed'
+  } else if (props.status==='Not Completed'){
+    statusClass = 'resolution__status--not-completed'
+  } else {
+    statusClass = 'resolution__status'
+  }
+
+  return(
+    <div className='resolution'>
+      <EditableH1 className='resolution__title' value={props.title} />
+      <EditableH3 className='resolution__desc' value={props.description} />
+      <p className={ statusClass }
+      >Status: {props.status}</p>
+      <div className='resolution__tag-container'>
+        Resolution Tags: { tags }
+      </div>
       <h5
-        className='delete'
+        className='resolution__remove'
         onClick={()=> props.removeResolution(props.id)}
-      >X</h5>
+      >Remove</h5>
     </div>
   );
 }
